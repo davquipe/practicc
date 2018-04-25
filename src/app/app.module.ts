1987d0b97138fe34fd911811226004abd09df4f3
davquipe <davidcondori99@gmail.com>
2018-04-25 16:12:45 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// Rutas
-import { APP_ROUTES } from './app.routes';
 
-// Modulos
-import { PagesModule } from './pages/pages.module';
-
-// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    NopagefoundComponent,
+    HomeComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
-    BrowserModule,
-    APP_ROUTES,
-    PagesModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
+  selector: 'app-home',
+  templateUrl: './home.component.html',
   styles: []
 })
-export class PagesComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
